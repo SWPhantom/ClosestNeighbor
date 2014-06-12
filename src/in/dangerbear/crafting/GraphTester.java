68a16b5c7f44c644460c9afe40d6c937ea9c1e10
Zhenya Frolov <hellcomestotown@hotmail.com>
2014-06-11 21:20:31 -0700

    Pulled timer code out into their own functions.

--- src/in/dangerbear/crafting/GraphTester.java
+++ src/in/dangerbear/crafting/GraphTester.java
@@ -4,24 +4,30 @@ import java.awt.Point;
 import java.util.ArrayList;
 
 public class GraphTester{
+	long startTime;
+	long stopTime;
+	long elapsedTime;
+	int[] closeness;
 	
-	public GraphTester(){
+	public GraphTester(ArrayList<Point> field1){
+		startTime = 0;
+		stopTime = 0;
+		elapsedTime = 0;
+		closeness = new int[field1.size()];
 	}
 
 	/**
 	 * method bruteForce
 	 * 
 	 * Does an n^2 scan through both of the given unsorted fields.
+	 * For every Point in field1, see how many field2 Points are <= distance.
 	 * 
 	 * @param field1
 	 * @param field2
 	 * @param distance
 	 */
-	public void bruteForce(ArrayList<Point> field1, ArrayList<Point> field2,
-			int distance){
-		int[] closeness = new int[field1.size()];
-		pr("Starting brute force algorithm.\n");
-		long startTime = System.currentTimeMillis();
+	public void bruteForce(ArrayList<Point> field1, ArrayList<Point> field2, int distance){
+		startTimer("Starting :: Brute Force.\n");
 		
 		double distanceSq = distance * distance;
 		for(int i = 0; i < field1.size(); ++i){
@@ -35,20 +41,12 @@ public class GraphTester{
 			}
 		}
 		
-		long stopTime = System.currentTimeMillis();
-		long elapsedTime = stopTime - startTime;
-		pr("Stopping brute force algorithm. Time (ms): " + elapsedTime + "\n");
-		for(int i = 0; i< closeness.length; ++i){
-			pr(closeness[i] + ", ");
-		}
-		pr("\n");
+		endTimer("Stopping :: Brute Force. ");
+		results();
 	}
 	
-	
-
-	public void sortedBruteForce(ArrayList<Point> field1, ArrayList<Point> field2,
-			int distance){
-		// TODO
+	public void sortedBruteForce(ArrayList<Point> field1, ArrayList<Point> field2, int distance){
+		
 	}
 
 	public void quickSelect(ArrayList<Point> field1, ArrayList<Point> field2,
@@ -61,6 +59,24 @@ public class GraphTester{
 			int distance){
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private void startTimer(String message){
+		pr(message);
+		startTime = System.currentTimeMillis();
+	}
+
+	private void endTimer(String message){
+		stopTime = System.currentTimeMillis();
+		elapsedTime = stopTime - startTime;
+		pr(message + "Time (ms): " + elapsedTime + "\n");
+	}
+
+	private void results(){
+		for(int i = 0; i< closeness.length; ++i){
+			pr(closeness[i] + ", ");
+		}
+		pr("\n");
 	}
 	
 	private void pr(String string){
